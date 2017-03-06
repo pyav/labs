d2a4eb94070d30c76cc7e50892f1af93a8e0af09
Anand Verma <verma.anand09@gmail.com>
2017-03-06 21:28:20 +0530

    Update level_order_traversal.c

--- c/data_structures/level_order_traversal.c
+++ c/data_structures/level_order_traversal.c
@@ -3,7 +3,7 @@
  *
  * Author: @pyav
  */
-	
+
 #include <stdio.h>
 #include <malloc.h>
 #include <stdlib.h>
@@ -20,67 +20,67 @@ int track_pos = -1;
 
 /* The node structure */
 struct node {
-	int data;
-	struct node *left;
-	struct node *right;
+  int data;
+  struct node *left;
+  struct node *right;
 };
 struct node *data_array[MAX_SIZE];
 
 /* Create new node and assign value */
 struct node *new_node(int data)
 {
-	struct node *one_node = NULL;
+  struct node *one_node = NULL;
 
-	one_node = (struct node *) malloc(sizeof (struct node));
-	if (one_node != NULL) {
-		one_node->data = data;
-		one_node->left = NULL;
-		one_node->right = NULL;
-	} else {
-		perror("malloc");
-	}
+  one_node = (struct node *) malloc(sizeof (struct node));
+  if (one_node != NULL) {
+    one_node->data = data;
+    one_node->left = NULL;
+    one_node->right = NULL;
+  } else {
+    perror("malloc");
+  }
 
-	return one_node;
+  return one_node;
 }
 
 /* Append nodes to the array, call the function recursively */
 void add_level_order(struct node *root) 
 {
-	if (NULL == root)
-		return;
+  if (NULL == root)
+    return;
 
-	data_array[++count] = root->left;
-	data_array[++count] = root->right;
-	track_pos = track_pos + 1;
-	add_level_order(data_array[track_pos]);
+  data_array[++count] = root->left;
+  data_array[++count] = root->right;
+  track_pos = track_pos + 1;
+  add_level_order(data_array[track_pos]);
 }
 
 /* Main driver code */
 int main(void)
 {
-	int i = 0;
-	struct node *root = NULL;
-	
-  /* Create the tree */
-	root = new_node(1);
-	root->left = new_node(2);
-	root->right= new_node(3);
-	root->left->left = new_node(4);
-	root->left->right = new_node(5);
+  int i = 0;
+  struct node *root = NULL;
 
-	data_array[++count] = root;
-	track_pos = track_pos + 1;
+  /* Create the tree */
+  root = new_node(1);
+  root->left = new_node(2);
+  root->right= new_node(3);
+  root->left->left = new_node(4);
+  root->left->right = new_node(5);
+
+  data_array[++count] = root;
+  track_pos = track_pos + 1;
 
   /* Recursive function call */
-	add_level_order(data_array[track_pos]);
+  add_level_order(data_array[track_pos]);
 
   /* Print the array */
   printf("\nData from nodes in level order: ");
-	for (i = 0; i < MAX_SIZE; i++)
-		printf("%d ", data_array[i]->data);
+  for (i = 0; i < MAX_SIZE; i++)
+    printf("%d ", data_array[i]->data);
 
-	printf("\n\n");
+  printf("\n\n");
 
-	return SUCCESS;
+  return SUCCESS;
 }
 
