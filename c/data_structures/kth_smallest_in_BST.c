580caff8e19e3f947fc4b7a95033db4592f70b46
Anand Verma <verma.anand09@gmail.com>
2017-03-07 20:59:26 +0530

    Update kth_smallest_in_BST.c

--- c/data_structures/kth_smallest_in_BST.c
+++ c/data_structures/kth_smallest_in_BST.c
@@ -12,48 +12,48 @@
 
 /* The node structure */
 struct node {
-	int data;
-	struct node *left;
-	struct node *right;
+  int data;
+  struct node *left;
+  struct node *right;
 };
 
 /* Create a new node */
 struct node *new_node(int data)
 {
-	struct node *one_node = NULL;
+  struct node *one_node = NULL;
 
-	one_node = (struct node *) malloc(sizeof (struct node));
-	if (one_node != NULL) {
-		one_node->data = data;
-		one_node->left = NULL;
-		one_node->right = NULL;
-	}
+  one_node = (struct node *) malloc(sizeof (struct node));
+  if (one_node != NULL) {
+    one_node->data = data;
+    one_node->left = NULL;
+    one_node->right = NULL;
+  }
 
-	return one_node;
+  return one_node;
 }
 
 /* Insert a node */
 struct node *insert_node(struct node *node, int data)
 {
-	if (node == NULL)
-		return new_node(data);
+  if (node == NULL)
+    return new_node(data);
 
-	if (data < node->data)
-		node->left = insert_node(node->left, data);
-	else if (data > node->data)
-		node->right = insert_node(node->right, data);
+  if (data < node->data)
+    node->left = insert_node(node->left, data);
+  else if (data > node->data)
+    node->right = insert_node(node->right, data);
 
-	return node;
+  return node;
 }
 
 /* Print tree inorder */
 void print_tree_inorder(struct node *node)
 {
-	if (node != NULL) {
-		print_tree_inorder(node->left);
-		printf("%d ", node->data);
-		print_tree_inorder(node->right);
-	}
+  if (node != NULL) {
+    print_tree_inorder(node->left);
+    printf("%d ", node->data);
+    print_tree_inorder(node->right);
+  }
 }
 
 static int count = 0;
@@ -61,57 +61,57 @@ static int count = 0;
 /* Find the kth smallest element */
 void find_kth_smallest_elem(struct node *node, int k)
 {
-	if (node == NULL)
-		return;
-	
-	find_kth_smallest_elem(node->left, k);
-	count++;
+  if (node == NULL)
+    return;
 
-	if (k == count) {
-		printf("\n%dth smallest value in the BST is: %d\n", k, node->data);
-		exit(EXIT_SUCCESS);
-	}
+  find_kth_smallest_elem(node->left, k);
+  count++;
 
-	find_kth_smallest_elem(node->right, k);
+  if (k == count) {
+    printf("\n%dth smallest value in the BST is: %d\n", k, node->data);
+    exit(EXIT_SUCCESS);
+  }
+
+  find_kth_smallest_elem(node->right, k);
 }
 
 /* Cleanup */
 void free_all(struct node **node)
 {
-	if (*node == NULL)
-		return;
-	
-	free_all(&(*node)->left);
-	free_all(&(*node)->right);
-	free(*node);
-	*node = NULL;
+  if (*node == NULL)
+    return;
+
+  free_all(&(*node)->left);
+  free_all(&(*node)->right);
+  free(*node);
+  *node = NULL;
 }
 
 /* Main driver program */
 int main(void)
 {
-	int k = -1;
-	struct node *root = NULL;
+  int k = -1;
+  struct node *root = NULL;
 
   /* Create the tree */
-	root = insert_node (root, 50);
-	insert_node (root, 10);
-	insert_node (root, 55);
-	insert_node (root, 70);
-	insert_node (root, 40);
-	insert_node (root, 30);
+  root = insert_node (root, 50);
+  insert_node (root, 10);
+  insert_node (root, 55);
+  insert_node (root, 70);
+  insert_node (root, 40);
+  insert_node (root, 30);
 
-	print_tree_inorder(root);
+  print_tree_inorder(root);
 
-	printf("\nEnter the value of k: ");
-	scanf("%d", &k);
+  printf("\nEnter the value of k: ");
+  scanf("%d", &k);
 
   /* Find the kth smallest element */
-	find_kth_smallest_elem(root, k);
+  find_kth_smallest_elem(root, k);
 
   /* Cleanup */
-	free_all(&root);
+  free_all(&root);
 
-	return SUCCESS;
+  return SUCCESS;
 }
 
