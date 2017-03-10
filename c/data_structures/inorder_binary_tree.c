a6325291d4ffae4f81191f5352ce98a13438e73e
Anand Verma <verma.anand09@gmail.com>
2017-03-10 19:47:24 +0530

    Update inorder_binary_tree.c

--- c/data_structures/inorder_binary_tree.c
+++ c/data_structures/inorder_binary_tree.c
@@ -1,20 +1,29 @@
+/*
+ * This program demonstrates inorder traversal of a Binary Search Tree.
+ * 
+ * Author: @pyav
+ */
+
 #include <stdio.h>
 #include <malloc.h>
 
 #define SUCCESS 0
 #define FAILURE 1
 
+/* Node structure */
 struct node {
 	int data;
 	struct node *left;
 	struct node *right;
 };
 
+/* Create a new node and return it */
 struct node *new_node(int data)
 {
 	struct node *one_node = NULL;
+
 	one_node = (struct node *) malloc(sizeof (struct node));
-	if (one_node != NULL) {
+	if (NULL != one_node) {
 		one_node->data = data;
 		one_node->left = NULL;
 		one_node->right = NULL;
@@ -23,32 +32,35 @@ struct node *new_node(int data)
 	return one_node;
 }
 
+/* Insert a node */
 struct node *insert_node(struct node *node, int data)
 {
-	if (node == NULL) {
+  /* Create a new node if the passed node is NULL */
+	if (NULL == node)
 		return new_node(data);
-	}
 
-	if (data < node->data) {
+	if (data < node->data)
 		node->left = insert_node(node->left, data);
-	} else if (data > node->data){
+  else if (data > node->data)
 		node->right = insert_node(node->right, data);
-	}
 
 	return node;
 }
 
+/* Traverse inorder and print the tree */
 void print_tree_inorder(struct node * node)
 {
-	if (node != NULL) {
+	if (NULL != node) {
 		print_tree_inorder(node->left);
 		printf("%d ", node->data);
 		print_tree_inorder(node->right);
 	}
 }
 
+/* Main driver */
 int main(void)
 {
+  /* Create the tree */
 	struct node *root = NULL;
 	root = insert_node(root, 50);
 	insert_node(root, 10);
@@ -57,6 +69,7 @@ int main(void)
 	insert_node(root, 40);
 	insert_node(root, 30);
 
+  /* Print the tree inorder */
 	print_tree_inorder(root);
 	printf ("\n");
 
