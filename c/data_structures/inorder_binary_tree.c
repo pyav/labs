20b71b3415242b5a108875a3f2034dbf0c502c6a
Anand Verma <verma.anand09@gmail.com>
2017-03-10 19:48:56 +0530

    Update inorder_binary_tree.c

--- c/data_structures/inorder_binary_tree.c
+++ c/data_structures/inorder_binary_tree.c
@@ -12,67 +12,67 @@
 
 /* Node structure */
 struct node {
-	int data;
-	struct node *left;
-	struct node *right;
+  int data;
+  struct node *left;
+  struct node *right;
 };
 
 /* Create a new node and return it */
 struct node *new_node(int data)
 {
-	struct node *one_node = NULL;
+  struct node *one_node = NULL;
 
-	one_node = (struct node *) malloc(sizeof (struct node));
-	if (NULL != one_node) {
-		one_node->data = data;
-		one_node->left = NULL;
-		one_node->right = NULL;
-	}
+  one_node = (struct node *) malloc(sizeof (struct node));
+  if (NULL != one_node) {
+    one_node->data = data;
+    one_node->left = NULL;
+    one_node->right = NULL;
+  }
 
-	return one_node;
+  return one_node;
 }
 
 /* Insert a node */
 struct node *insert_node(struct node *node, int data)
 {
   /* Create a new node if the passed node is NULL */
-	if (NULL == node)
-		return new_node(data);
+  if (NULL == node)
+    return new_node(data);
 
-	if (data < node->data)
-		node->left = insert_node(node->left, data);
+  if (data < node->data)
+    node->left = insert_node(node->left, data);
   else if (data > node->data)
-		node->right = insert_node(node->right, data);
+    node->right = insert_node(node->right, data);
 
-	return node;
+  return node;
 }
 
 /* Traverse inorder and print the tree */
 void print_tree_inorder(struct node * node)
 {
-	if (NULL != node) {
-		print_tree_inorder(node->left);
-		printf("%d ", node->data);
-		print_tree_inorder(node->right);
-	}
+  if (NULL != node) {
+    print_tree_inorder(node->left);
+    printf("%d ", node->data);
+    print_tree_inorder(node->right);
+  }
 }
 
 /* Main driver */
 int main(void)
 {
   /* Create the tree */
-	struct node *root = NULL;
-	root = insert_node(root, 50);
-	insert_node(root, 10);
-	insert_node(root, 55);
-	insert_node(root, 70);
-	insert_node(root, 40);
-	insert_node(root, 30);
+  struct node *root = NULL;
+  root = insert_node(root, 50);
+  insert_node(root, 10);
+  insert_node(root, 55);
+  insert_node(root, 70);
+  insert_node(root, 40);
+  insert_node(root, 30);
 
   /* Print the tree inorder */
-	print_tree_inorder(root);
-	printf ("\n");
+  print_tree_inorder(root);
+  printf ("\n");
 
-	return SUCCESS;
+  return SUCCESS;
 }
 
