8db8db827763d2d60aa06ecab43cfb2889df3cf0
Anand Verma <verma.anand09@gmail.com>
2017-03-11 21:41:35 +0530

    Update count_leaf_nodes.c

--- data_structures/count_leaf_nodes.c
+++ data_structures/count_leaf_nodes.c
@@ -18,36 +18,36 @@
 
 /* Node structure */
 struct node {
-	int data;
-	struct node *left;
-	struct node *right;
+  int data;
+  struct node *left;
+  struct node *right;
 };
 
 /* Create a new node */
 struct node *new_node(int data) 
 {
-	struct node *one_node = (struct node *) malloc(sizeof (struct node));
+  struct node *one_node = (struct node *) malloc(sizeof (struct node));
   if (NULL == one_node) {
     perror("malloc");
     exit(EXIT_FAILURE);
   }
 
-	one_node->data = data;
-	one_node->left = NULL;
-	one_node->right = NULL;
+  one_node->data = data;
+  one_node->left = NULL;
+  one_node->right = NULL;
 
-	return one_node;
+  return one_node;
 }
 
 /* Count the leaf nodes */
 int count_leaf_nodes(struct node *temp_node)
 {
-	if (temp_node == NULL)
-		return 0;
+  if (temp_node == NULL)
+    return 0;
 
   /* For leaf node, return count 1 */
-	if (temp_node->left == NULL && temp_node->right == NULL)
-		return 1;
+  if (temp_node->left == NULL && temp_node->right == NULL)
+    return 1;
 
   /* Count the leaf nodes recursively */
   return (count_leaf_nodes(temp_node->left) + 
@@ -57,22 +57,22 @@ int count_leaf_nodes(struct node *temp_node)
 /* Main driver code */
 int main(void)
 {
-	int total_leaf_nodes = -1;
+  int total_leaf_nodes = -1;
 
   /* Create the tree structure */
-	struct node *root = new_node(1);
-	root->left = new_node(2);
-	root->right = new_node(3);
-	root->left->left = new_node(4);
-	root->left->right = new_node(5);
-	root->right->left = new_node(6);
-	root->right->right = new_node(7);
+  struct node *root = new_node(1);
+  root->left = new_node(2);
+  root->right = new_node(3);
+  root->left->left = new_node(4);
+  root->left->right = new_node(5);
+  root->right->left = new_node(6);
+  root->right->right = new_node(7);
 
   /* Call function to count leaf nodes */
-	total_leaf_nodes = count_leaf_nodes(root);
-	printf("\nTotal leaf nodes = %d\n\n", total_leaf_nodes);
+  total_leaf_nodes = count_leaf_nodes(root);
+  printf("\nTotal leaf nodes = %d\n\n", total_leaf_nodes);
 
-	return SUCCESS;
+  return SUCCESS;
 }
 /* Main */
 
