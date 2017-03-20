066286d2ad6b2be7e798243d79de86e52e0f112b
Anand Verma <verma.anand09@gmail.com>
2017-03-20 09:07:57 +0530

    update sorted_print_array_BST.c

--- data_structures/sorted_print_array_BST.c
+++ data_structures/sorted_print_array_BST.c
@@ -1,15 +1,18 @@
 /* 
- * This program prints array which represents a BST
- * in sorted order
+ * This program prints array which represents a BST in sorted order
+ *
+ * Output
+ * ------
+ *
+ * 1 2 3 4 5 
+ *
  *
  * Author: @pyav
  */
 
-
 #include <stdio.h>
 
 #define SUCCESS 0
-#define FAILURE 1
 
 /* Print in sorted form */
 void print_sorted(int arr[], int low, int high)
@@ -17,18 +20,23 @@ void print_sorted(int arr[], int low, int high)
 	if (low > high)
 		return;
 	
-	print_sorted(arr, low*2 + 1, high);
+	print_sorted(arr, (low * 2) + 1, high);
 	printf("%d ", arr[low]);
-	print_sorted(arr, low*2 + 2, high);
+	print_sorted(arr, (low * 2) + 2, high);
 }
 
-/* Main driver program */
+/* Main driver code */
 int main (void)
 {
+  /* Array representing data in a BST level-wise */
 	int arr[] = {4,2,5,1,3};
-	int arr_siz = sizeof(arr) / sizeof(arr[0]);
 
-	print_sorted(arr, 0, arr_siz - 1);
+  /* Calculate number of elements */
+	int no_elems = sizeof(arr) / sizeof(arr[0]);
+
+  /* Print in sorted order, send last index of array */
+	print_sorted(arr, 0, no_elems - 1);
+
 	printf("\n");
 
 	return SUCCESS;
