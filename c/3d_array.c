dcb877eb0688c339182731c7526f0ede3d030cc5
Anand Verma <verma.anand09@gmail.com>
2017-03-30 08:39:29 +0530

    add 3d_array.c

---
+++ c/3d_array.c
@@ -0,0 +1,46 @@
+/*
+ * Following code demonstrates 3D-array usage. 
+ *
+ * Output
+ * ------
+ *
+ * $ gcc -Wall 3d_array.c && ./a.out 
+ * first second third 
+ * fourth fifth sixth 
+ *
+ *
+ * Author
+ * ------
+ *
+ * pyav
+ ******************************************************************************/
+
+#include <stdio.h>
+
+#define SUCCESS 0
+#define MAX_FIRST_D 2
+#define MAX_SECOND_D 3
+#define MAX_THIRD_D 10
+
+int main(void)
+{
+  int i = 0; 
+  int j = 0; 
+
+  /* Initialize the array */
+  char arr[MAX_FIRST_D][MAX_SECOND_D][MAX_THIRD_D] = {
+    {"first", "second", "third"},
+    {"fourth", "fifth", "sixth"},
+  };
+
+  /* Print the array */
+  for (i = 0; i < MAX_FIRST_D; i++) {
+    for (j = 0; j < MAX_SECOND_D; j++)
+      printf("%s ", arr[i][j]);
+
+    printf("\n");
+  }
+
+  return SUCCESS;
+}
+
