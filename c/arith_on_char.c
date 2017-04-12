04e98343ffc33f120040694e97322c74785c9e83
Anand Verma <verma.anand09@gmail.com>
2017-04-12 09:09:07 +0530

    add arith_on_char.c

---
+++ c/arith_on_char.c
@@ -0,0 +1,33 @@
+/*
+ * Following code demonstrates arithmatic operation on char variables.
+ *
+ * Output
+ * ------
+ *
+ * a = 30, b = 40, c =  10
+ * d = (a * b) / c = 120
+ * sizeof a = 1, sizeof b = 1, sizeof c = 1
+ *
+ *
+ * Author
+ * ------
+ *
+ * pyav
+ ******************************************************************************/
+
+#include <stdio.h>
+
+#define SUCCESS 0
+
+int main(void)
+{
+  char a = 30, b = 40, c = 10;
+  char d = (a * b) / c;
+
+  printf("\na = %d, b = %d, c =  %d\n", a, b, c);
+  printf("d = (a * b) / c = %d\n", d);
+  printf("sizeof a = %d, sizeof b = %d, sizeof c = %d\n\n", sizeof a, sizeof b,
+         sizeof c);
+
+  return SUCCESS;
+}
