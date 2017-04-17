edfde0e2d2b1410dff468ceb6e3363fb079ec574
Anand Verma <verma.anand09@gmail.com>
2017-04-17 09:50:10 +0530

    add comma.c

---
+++ c/comma.c
@@ -0,0 +1,44 @@
+/*
+ * Following program demonstrates printing of multiple assignments, increment
+ * sequence and printing the printf returned value. Note that it compiles with a
+ * warning as given below in the 'Output' section which is the actual output
+ * obtained by executing the object file (./a.out).
+ *
+ *
+ * Output
+ * ------
+ *
+ * left-hand operand of comma expression has no effect [-Wunused-value]
+ *
+ * x = 10, y = 20, Final printed value = 20
+ * y = 12
+ * z = 7
+ *
+ *
+ * Author
+ * ------
+ *
+ * pyav
+ ******************************************************************************/
+
+#include <stdio.h>
+
+#define SUCCESS 0
+
+int main(void)
+{
+  int x = 10;
+  int y = 20;
+  int z;
+
+  printf("\nx = 10, y = 20, Final printed value = %d\n", (x, y));
+
+  y = (x++, ++x);
+
+  z = printf("y = %d\n", y);
+
+  printf("z = %d\n\n", z);
+
+  return SUCCESS;
+}
+
