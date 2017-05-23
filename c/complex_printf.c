89563c8f17a36bb4b61682c9bbb515e4de1b0e88
Anand Verma <verma.anand09@gmail.com>
2017-05-23 09:01:39 +0530

    add complex_printf.c

---
+++ c/complex_printf.c
@@ -0,0 +1,29 @@
+/*
+ * Following program demonstrates multiple prints in a single assignment. It
+ * also shows that the length of the last print (nearest to the semi-colon) is
+ * assigned to the variable. 
+ * 
+ *
+ * Output
+ * ------
+ *
+ * Pyav1234
+ * 4
+ *
+ *
+ * Author
+ * ------
+ *
+ * pyav
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int x = (printf("Pyav"), printf("1234"));
+
+	printf ("\n%d\n", x);
+
+	return 0;
+}
