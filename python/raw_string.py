a0fff1c0fc8a0116a49b245bfdcc908bed9fe1f7
Anand Verma <verma.anand09@gmail.com>
2018-02-24 21:26:49 +0530

    python/raw_string.py

---
+++ python/raw_string.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+'''
+
+__author__ = 'pyav'
+
+Command
+-------
+python3 raw_string.py
+
+Output
+------
+a
+umber
+a\number
+
+'''
+
+print('a\number')
+print(r'a\number')
+
