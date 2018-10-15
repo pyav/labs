e22ce11a0c98dcc28be925d19e7083b735a5b211
Anand Verma <verma.anand09@gmail.com>
2018-10-15 23:09:25 +0530

    add singleton

---
+++ designPatterns/CreationalPatterns/Singleton.java
@@ -0,0 +1,25 @@
+/**
+ * Following program shows the Singleton pattern with lazy-initialization.
+ * 
+ * @author pyav 
+ */
+
+package designPatterns.CreationalPatterns;
+
+public final class Singleton {
+	private static volatile Singleton instance = null;
+
+	private Singleton() {}
+
+	public static Singleton getInstance() {
+		if (instance == null) {
+			synchronized (Singleton.class) {
+				if (instance == null) {
+					instance = new Singleton();
+				}
+			}
+		}
+		
+		return instance;
+	}
+}
