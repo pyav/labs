c35600955a685e693cbd096987b06af44df5d3a4
pyav <verma.anand09@gmail.com>
2019-09-18 20:59:16 +0530

    Add BinaryTree.java

---
+++ data structures/BinaryTree.java
@@ -0,0 +1,64 @@
+/**
+ * Following program demonstrates a sample binary tree construction and traversal.
+ * 
+ * Output
+ * ------
+ * 1
+ * 2
+ * Node is null.
+ * Node is null.
+ * 3
+ * Node is null.
+ * Node is null.
+ *
+ * 
+ * @author aanverma
+ */
+
+class OneNode {
+	int data;
+	OneNode left;
+	OneNode right;
+
+	OneNode(int val) {
+		data = val;
+		left = null;
+		right = null;
+	}
+}
+
+class TreeStructure {
+
+	OneNode node;
+
+	TreeStructure() {
+		node = null;
+	}
+
+	public static void printTreeStructure(OneNode node) {
+
+		if (node == null) {
+			System.out.println("Node is null.");
+			return;
+		}
+
+		System.out.println(node.data);
+		printTreeStructure(node.left);
+		printTreeStructure(node.right);
+	}
+
+}
+
+public class BinaryTree {
+
+	public static void main(String[] args) {
+
+		TreeStructure root = new TreeStructure();
+		root.node = new OneNode(1);
+		root.node.left = new OneNode(2);
+		root.node.right = new OneNode(3);
+
+		TreeStructure.printTreeStructure(root.node);
+	}
+
+}
