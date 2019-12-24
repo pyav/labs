c83fd6fc03f7bc88513e7735036a300725436181
pyav <verma.anand09@gmail.com>
2019-12-24 08:30:46 +0530

    add CustomTrim.java

---
+++ java/CustomTrim.java
@@ -0,0 +1,17 @@
+/**
+ * Output
+ * ------
+ * s==s
+ *
+ * @author pyav
+ */
+
+public class CustomTrim {
+
+	public static void main(String[] args) {
+
+		String s = "    ".trim();
+		System.out.println("s=" + s + "=s");
+	}
+
+}
