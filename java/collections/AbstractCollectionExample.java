21cffdb40d4f3eedc7780df45924cd3337d65180
pyav <verma.anand09@gmail.com>
2020-01-28 21:07:18 +0530

    add AbstractCollectionExample.java

---
+++ java/collections/AbstractCollectionExample.java
@@ -0,0 +1,34 @@
+/**
+ * Following code shows a sample example of AbstractCollection.
+ * 
+ * Output
+ * ------
+ * [First, Second, Third]
+ * [First, Second, Third]
+ * absCol = [].
+ */
+
+import java.util.AbstractCollection;
+import java.util.TreeSet;
+
+public class AbstractCollectionExample {
+
+	public static void main(String[] args) {
+
+		AbstractCollection<String> absCol = new TreeSet<String>();
+		absCol.add("First");
+		absCol.add("Third");
+		absCol.add("Second");
+		absCol.add("First");
+		
+		System.out.println(absCol);
+		
+		AbstractCollection<String> absCol2 = new TreeSet<String>();
+		absCol2.addAll(absCol);
+		System.out.println(absCol2);
+		
+		absCol.clear();
+		System.out.println("absCol = "+absCol.toString()+".");
+	}
+
+}
