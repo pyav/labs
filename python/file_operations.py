80aad69f98f72114ebda06a2a697b77d1f77220f
pyav <verma.anand09@gmail.com>
2020-06-30 23:28:50 +0530

    update file operations and test files

--- python/file_operations.py
+++ python/file_operations.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
-with open("/Users/aanverma/Desktop/labs/python/test_file.txt", "r") as f:
+with open("test_file.txt", "r") as f:
     data = f.read()
     f.close()
 
 data=data.replace("Anand", "pyav")
 
-with open("/Users/aanverma/Desktop/labs/python/new_test_file.txt", "w") as f:
+with open("new_test_file.txt", "w") as f:
     f.write(data)
     f.close()
+
