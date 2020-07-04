ebf040c93b74a39660b73903181dadb403649b2b
pyav <verma.anand09@gmail.com>
2020-07-04 20:20:09 +0530

    add SampleAutoWiredExample.java

---
+++ spring/coursera/SampleAutoWiredExample.java
@@ -0,0 +1,37 @@
+package spring.coursera;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+
+class BeanClass {
+
+}
+
+@Controller
+class VideoSvc {
+
+	@Autowired
+	public void auroWired(BeanClass beanClass) {
+
+	}
+}
+
+@Configuration
+class ConfigurationProvider {
+
+	@Bean
+	public BeanClass bean() {
+		return new BeanClass();
+	}
+
+}
+
+public class SampleAutoWiredExample {
+
+	public static void main(String[] args) {
+
+	}
+
+}
