355762f3b25a15067035daea99f57241db6af4a2
pyav <verma.anand09@gmail.com>
2020-11-30 09:16:24 +0530

    add InorderTraversal.java

---
+++ leetcode/InorderTraversal.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+import java.util.List;
+import java.util.ArrayList;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class InorderTraversal {
+
+    private List<Integer> list = new ArrayList<Integer>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return list;
+        }
+        
+        inorderTraversal(root.left);
+        list.add(root.val);
+        inorderTraversal(root.right);
+        return list;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.right = new TreeNode(3);
+
+        List<Integer> list = new InorderTraversal().inorderTraversal(root);
+        for (int i = 0; i<list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+}
+
