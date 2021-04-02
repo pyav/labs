f1e99a67799fcab4a0b42d982234c2f5505d7b8d
pyav <verma.anand09@gmail.com>
2021-04-02 23:17:18 +0530

    update EmumTest

--- javaexample/EnumTest.java
+++ javaexample/EnumTest.java
@@ -1,17 +1,27 @@
+/**
+ * Output:
+ * ------
+ * SUCCESS
+ * FAILURE
+ * WARNING
+ * WARNING
+ */
+
+package javaexample;
 
 public class EnumTest {
 
 	public enum RetVal {
-		SUCCESS,
-		FAILURE,
-		WARNING
+		SUCCESS, FAILURE, WARNING
 	};
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println(RetVal.SUCCESS);
 		System.out.println(RetVal.FAILURE);
 		System.out.println(RetVal.WARNING);
+
+		System.out.println(RetVal.WARNING.toString());
 
 	}
 
