d0f7287bcf282680273582125bc2ffb126a8f55c
pyav <verma.anand09@gmail.com>
2021-05-27 07:33:07 +0530

    SubtractMinus.java

---
+++ src/algorithms/misc/SubtractMinus.java
@@ -0,0 +1,16 @@
+/**
+ * Output:
+ * ------
+ * 3
+ */
+
+package misc;
+
+public class SubtractMinus {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = -2;
+        System.out.println(a-b);
+    }
+}
+
