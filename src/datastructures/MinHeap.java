0514bd0fee87fc2e8a9e72fc397cd930b595dae5
Anand <verma.anand09@gmail.com>
2021-06-17 21:19:05 +0530

    remove unwanted comments

--- src/datastructures/MinHeap.java
+++ src/datastructures/MinHeap.java
@@ -36,185 +36,157 @@ public class MinHeap {
     int capacity;
     int heapSize;
     int heapArray[];
-    
-    // Constructor
+
     MinHeap(int capacity) {
-	this.capacity = capacity;
-	this.heapSize = 0;
-	this.heapArray = new int[this.capacity];
+        this.capacity = capacity;
+        this.heapSize = 0;
+        this.heapArray = new int[this.capacity];
     }
-    
-    // The swap method
+
     void swap(int heapArray[], int a, int b) {
-	int temp = heapArray[b];
-	heapArray[b] = heapArray[a];
-	heapArray[a] = temp;
+        int temp = heapArray[b];
+        heapArray[b] = heapArray[a];
+        heapArray[a] = temp;
     }
-    
-    // Return parent
+
     int parent(int i) {
-	return (i - 1) / 2;
+        return (i - 1) / 2;
     }
-    
-    // Return left child
+
     int leftChild(int i) {
-	return (2 * i) + 1;
+        return (2 * i) + 1;
     }
-    
-    // Return right child
+
     int rightChild(int i) {
-	return (2 * i) + 2;
+        return (2 * i) + 2;
     }
 
-    // Get the minimum value
     int getMin() {
-	return heapArray[0];
+        return heapArray[0];
     }
-    
-    // Extract the minimum value
+
     int extractMin() {
-	if (heapSize <= 0) {
-	    return Integer.MAX_VALUE;
-	}
-	
-	if (heapSize == 1) {
-	    return heapArray[--heapSize];
-	}
-	
-	// Save the minimum value and delete it. Decrease heap size.
-	int min = heapArray[0];
-	heapArray[0] = heapArray[heapSize - 1];
-	
-	/* 
-	 * A copy of value at the end should not be there, even if the 
-	 * heapSize controls the length of the heap, as this is an array 
-	 * representation.  
-	 */
-	heapArray[heapSize - 1] = 0;
-	
-	// Update the heap size
-	heapSize -= 1;
-	
-	// Heapify
-	minHeapify(0);
-	
-	// Return the minimum value
-	return min;
+        if (heapSize <= 0) {
+            return Integer.MAX_VALUE;
+        }
+
+        if (heapSize == 1) {
+            return heapArray[--heapSize];
+        }
+
+        int min = heapArray[0];
+        heapArray[0] = heapArray[heapSize - 1];
+
+        /* 
+         * A copy of value at the end should not be there, even if the 
+         * heapSize controls the length of the heap, as this is an array 
+         * representation.  
+         */
+        heapArray[heapSize - 1] = 0;
+        heapSize -= 1;
+        minHeapify(0);
+
+        return min;
     }
-    
-    // Heapify
+
     void minHeapify(int index) {
-	int left = leftChild(index);
-	int right = rightChild(index);
-	int min = index;
-	
-	if ((left < heapSize) && (heapArray[left] < heapArray[min])) {
-	    min = left;
-	}
+        int left = leftChild(index);
+        int right = rightChild(index);
+        int min = index;
 
-	if ((right < heapSize) && (heapArray[right] < heapArray[min])) {
-	    min = right;
-	}
-	
-	if (min != index) {
-	    swap(heapArray, index, min);
-	    minHeapify(min);
-	}
+        if ((left < heapSize) && (heapArray[left] < heapArray[min])) {
+            min = left;
+        }
+
+        if ((right < heapSize) && (heapArray[right] < heapArray[min])) {
+            min = right;
+        }
+
+        if (min != index) {
+            swap(heapArray, index, min);
+            minHeapify(min);
+        }
     }
-    
-    // Change the value of the element at 'index' and assign it to 'newVal'
+
     void decreaseKey(int index, int newVal) {
-	if (index > heapSize || index < 0) {
-	    System.out.println("ERROR: Index out of range");
-	    return;
-	}
-	
-	// Change the value at the index
-	heapArray[index] = newVal;
-	
-	int p = parent(index);
-	while ((index != 0) && (heapArray[p] > heapArray[index])) {
-	    swap(heapArray, p, index);
-	    index = p;
-	}
-    }
-    
-    // Delete a key
-    void deleteKey(int index) {
-	decreaseKey(index, Integer.MIN_VALUE);
-	extractMin();
-    }
-    
-    // Insert an element
-    void insertKey(int key) {
-	int i = 0;
-	
-	if (heapSize >= capacity) {
-	    System.out.println("ERROR: Heap array overflow");
-	}
-	
-	// Enter the element
-	heapSize += 1;
-	i = heapSize - 1;
-	heapArray[i] = key;
-	
-	// Stabilize if heap property is violated
-	while (i >= 0 && heapArray[parent(i)] > heapArray[i]) {
-	    swap(heapArray, parent(i), i);
-	    i = parent(i);
-	}
-    }
-    
-    public static void main(String[] args) {
-	
-	MinHeap obj = new MinHeap(11);
-	
-	// Call insert and delete methods
-	obj.insertKey(3);
-	obj.insertKey(2);
-	
-	// Print heap array
-	System.out.println("Heap Array:");
-	for (int i : obj.heapArray) {
-	    System.out.printf("%d ", i);
-	}
-	
-	System.out.println("\n\nDeleting the value at index '1'...");
-	obj.deleteKey(1); // Deletes '3'
-	
-	System.out.println("Inserting 15, 5, 4, 45...");
-	obj.insertKey(15);
-	obj.insertKey(5);
-	obj.insertKey(4);
-	obj.insertKey(45);
-	
-	System.out.println("\nHeap Array:");
-	for (int i : obj.heapArray) {
-	    System.out.printf("%d ", i);
-	}
-	
-	// Other Heap functionalities
-	System.out.printf("\n\nGet Min = %d", obj.getMin());
-	System.out.printf("\nExtract min = %d\n\n", obj.extractMin());
-	
-	System.out.println("Heap Array:");
-	for (int i : obj.heapArray) {
-	    System.out.printf("%d ", i);
-	}
-	
-	System.out.println("\n\nDecreasing (2,1):...");
-	
-	// Change the value at index '2'
-	obj.decreaseKey(2, 1); 
-	
-	System.out.printf("Get Min = %d\n", obj.getMin());
+        if (index > heapSize || index < 0) {
+            System.out.println("ERROR: Index out of range");
+            return;
+        }
 
-	System.out.println("\nHeap Array:");
-	for (int i : obj.heapArray) {
-	    System.out.printf("%d ", i);
-	}
-	
-	System.out.printf("\n\nHeap Size = %d", obj.heapSize);
+        heapArray[index] = newVal;
+        int p = parent(index);
+        while ((index != 0) && (heapArray[p] > heapArray[index])) {
+            swap(heapArray, p, index);
+            index = p;
+        }
+    }
+
+    void deleteKey(int index) {
+        decreaseKey(index, Integer.MIN_VALUE);
+        extractMin();
+    }
+
+    void insertKey(int key) {
+        int i = 0;
+
+        if (heapSize >= capacity) {
+            System.out.println("ERROR: Heap array overflow");
+        }
+
+        heapSize += 1;
+        i = heapSize - 1;
+        heapArray[i] = key;
+
+        while (i >= 0 && heapArray[parent(i)] > heapArray[i]) {
+            swap(heapArray, parent(i), i);
+            i = parent(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        MinHeap obj = new MinHeap(11);
+        obj.insertKey(3);
+        obj.insertKey(2);
+        System.out.println("Heap Array:");
+        for (int i : obj.heapArray) {
+            System.out.printf("%d ", i);
+        }
+
+        System.out.println("\n\nDeleting the value at index '1'...");
+        obj.deleteKey(1);
+
+        System.out.println("Inserting 15, 5, 4, 45...");
+        obj.insertKey(15);
+        obj.insertKey(5);
+        obj.insertKey(4);
+        obj.insertKey(45);
+
+        System.out.println("\nHeap Array:");
+        for (int i : obj.heapArray) {
+            System.out.printf("%d ", i);
+        }
+
+        System.out.printf("\n\nGet Min = %d", obj.getMin());
+        System.out.printf("\nExtract min = %d\n\n", obj.extractMin());
+
+        System.out.println("Heap Array:");
+        for (int i : obj.heapArray) {
+            System.out.printf("%d ", i);
+        }
+
+        System.out.println("\n\nDecreasing (2,1):...");
+
+        obj.decreaseKey(2, 1); 
+
+        System.out.printf("Get Min = %d\n", obj.getMin());
+
+        System.out.println("\nHeap Array:");
+        for (int i : obj.heapArray) {
+            System.out.printf("%d ", i);
+        }
+
+        System.out.printf("\n\nHeap Size = %d", obj.heapSize);
     }
 
 }
