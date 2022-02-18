bee0f3310ab9e969aac577eea25fce25db7556bd
Anand Verma <verma.anand09@gmail.com>
2022-02-18 10:04:49 +0530

    Leetcode array easy

---
+++ src/leetcode/ConcatenationOfArray.java
@@ -0,0 +1,30 @@
+/**
+ * https://leetcode.com/problems/concatenation-of-array/
+ *
+ * Output:
+ * ------
+ *
+ */
+
+package leetcode;
+
+public class ConcatenationOfArray {
+
+	public int[] getConcatenation(int[] nums) {
+	    int n = nums.length;
+        int[] ans = new int[2*n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = ans[i+n] = nums[i];
+        }
+        return ans;
+    }
+
+	public static void main(String[] args) {
+	    int[] arr = {1,2,1};
+		int[] res = new ConcatenationOfArray().getConcatenation();
+
+		for (int i = 0; i < res.length; i++) {
+			System.out.printf("%d ", res[i]);
+		}
+	}
+}
