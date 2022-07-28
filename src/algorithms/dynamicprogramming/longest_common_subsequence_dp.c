151aa16be2efccb999d9bff0117686a4f704e704
pyav <verma.anand09@gmail.com>
2022-07-28 21:17:25 +0530

    update lcs code

--- src/algorithms/dynamicprogramming/longest_common_subsequence_dp.c
+++ src/algorithms/dynamicprogramming/longest_common_subsequence_dp.c
@@ -22,79 +22,79 @@
 
 int max(int a, int b)
 {
-  return (a >= b)? a: b;
+    return (a >= b)? a: b;
 }
 
 /* Function to evaluate the longest common subsequence */
 int lcs(char *X, char *Y, size_t len_X, size_t len_Y)
 {
-  int i = 0;
-  int j = 0;
-  int c[len_X + 1][len_Y + 1];
+    int i = 0;
+    int j = 0;
+    int c[len_X + 1][len_Y + 1];
 
-  for (j = 0; j <= len_Y; j++)
-    c[i][j] = 0;
+    for (j = 0; j <= len_Y; j++)
+        c[i][j] = 0;
 
-  j = 0;
+    j = 0;
 
-  for (i = 0; i <= len_X; i++)
-    c[i][j] = 0;
+    for (i = 0; i <= len_X; i++)
+        c[i][j] = 0;
 
-  for (i = 1; i <= len_X; i++) {
-    for (j = 1; j <= len_Y; j++) {
-      if (X[i] == Y[j]) {
-        c[i][j] = 1 + c[i - 1][j  - 1];
-      } else {
-        c[i][j] = max(c[i - 1][j], c[i][j - 1]);
-      }
+    for (i = 1; i <= len_X; i++) {
+        for (j = 1; j <= len_Y; j++) {
+            if (X[i] == Y[j]) {
+                c[i][j] = 1 + c[i - 1][j  - 1];
+            } else {
+                c[i][j] = max(c[i - 1][j], c[i][j - 1]);
+            }
+        }
     }
-  }
 
-  /* Find the longest common subsequence */
-  int index = c[len_X][len_Y];
-  char *lcs = (char *) malloc((1 + index) * sizeof (char));
+    /* Find the longest common subsequence */
+    int index = c[len_X][len_Y];
+    char *lcs = (char *) malloc((1 + index) * sizeof (char));
 
-  /* Validate */
-  if (NULL == lcs) {
-    perror("malloc");
-    return FUNC_FAILURE;
-  }
-
-  lcs[index] = '\0'; /* Make it a string */
-
-  i = len_X;
-  j = len_Y;
-
-  while (i > 0 && j > 0) {
-    if (X[i - 1] == Y[j - 1]) {
-      lcs[index - 1] = X[i - 1];
-      index--;
-      i--;
-      j--;
-    } else if (c[i - 1][j] > c[i][j - 1]) {
-      i--;
-    } else {
-      j--;
+    /* Validate */
+    if (NULL == lcs) {
+        perror("malloc");
+        return FUNC_FAILURE;
     }
-  }
 
-  printf("\nThe lcs of %s and %s is: %s\n\n", X, Y, lcs);
+    lcs[index] = '\0'; /* Make it a string */
 
-  free(lcs);
-  return SUCCESS;
+    i = len_X;
+    j = len_Y;
+
+    while (i > 0 && j > 0) {
+        if (X[i - 1] == Y[j - 1]) {
+            lcs[index - 1] = X[i - 1];
+            index--;
+            i--;
+            j--;
+        } else if (c[i - 1][j] > c[i][j - 1]) {
+            i--;
+        } else {
+            j--;
+        }
+    }
+
+    printf("\nThe lcs of %s and %s is: %s\n\n", X, Y, lcs);
+
+    free(lcs);
+    return SUCCESS;
 } /* End of lcs() */
 
 /* Main driver code */
 int main(void)
 {
-  char *X = "AGGTAB";
-  char *Y = "GXTXAYB";
-  size_t len_X = strlen(X);
-  size_t len_Y = strlen(Y);
+    char *X = "AGGTAB";
+    char *Y = "GXTXAYB";
+    size_t len_X = strlen(X);
+    size_t len_Y = strlen(Y);
 
-  if (SUCCESS == lcs(X, Y, len_X, len_Y))
-    return SUCCESS;
-  else 
-    return FAILURE;
+    if (SUCCESS == lcs(X, Y, len_X, len_Y))
+        return SUCCESS;
+    else 
+        return FAILURE;
 }
 
