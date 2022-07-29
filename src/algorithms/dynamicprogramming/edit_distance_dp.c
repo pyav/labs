9cae3189e5740c3c8e5cbfa3990a00060f12d11c
pyav <verma.anand09@gmail.com>
2022-07-29 20:48:03 +0530

    update minor

--- src/algorithms/dynamicprogramming/edit_distance_dp.c
+++ src/algorithms/dynamicprogramming/edit_distance_dp.c
@@ -30,7 +30,7 @@
 /* Minimum of two */
 int min(int a, int b)
 {
-  return (a < b)? a: b;
+    return (a < b)? a: b;
 }
 
 /* Least among three */
@@ -39,39 +39,39 @@ int min(int a, int b)
 /* Calculate and return the edit distance value using dp */
 int edit_dist_dp(char *str1, char *str2, size_t l1, size_t l2)
 {
-  int i = 0;
-  int j = 0;
-  int E[l1 + 1][l2 + 1];
+    int i = 0;
+    int j = 0;
+    int E[l1 + 1][l2 + 1];
 
-  for (j = 0; j <= l2; j++)
-    E[0][j] = j;
+    for (j = 0; j <= l2; j++)
+        E[0][j] = j;
 
-  for (i = 0; i <= l1; i++)
-    E[i][0] = i;
+    for (i = 0; i <= l1; i++)
+        E[i][0] = i;
 
-  for (i = 1; i <= l1; i++)
-    for (j = 1; j <= l2; j++)
-      if (str1[i - 1] == str2[j - 1])
-        E[i][j] = E[i - 1][j - 1];
-      else
-        E[i][j] = 1 + min3(E[i][j - 1], E[i - 1][j], E[i - 1][j - 1]); 
+    for (i = 1; i <= l1; i++)
+        for (j = 1; j <= l2; j++)
+            if (str1[i - 1] == str2[j - 1])
+                E[i][j] = E[i - 1][j - 1];
+            else
+                E[i][j] = 1 + min3(E[i][j - 1], E[i - 1][j], E[i - 1][j - 1]); 
 
-  return E[l1][l2];
+    return E[l1][l2];
 }
 
 /* Main driver code */
 int main(void)
 {
-  int edit_distance = -1;
-  char *str1 = "pyavgithub";
-  char *str2 = "pYavghub";
+    int edit_distance = -1;
+    char *str1 = "pyavgithub";
+    char *str2 = "pYavghub";
 
-  size_t l1 = strlen(str1);
-  size_t l2 = strlen(str2);
+    size_t l1 = strlen(str1);
+    size_t l2 = strlen(str2);
 
-  edit_distance = edit_dist_dp(str1, str2, l1, l2);
-  printf("Edit distance of %s & %s = %d\n", str1, str2, edit_distance);
+    edit_distance = edit_dist_dp(str1, str2, l1, l2);
+    printf("Edit distance of %s & %s = %d\n", str1, str2, edit_distance);
 
-  return SUCCESS;
+    return SUCCESS;
 }
 
