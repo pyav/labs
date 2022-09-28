a3dd958a9a37c20f8700d5c364464530661ed1c8
pyav <verma.anand09@gmail.com>
2022-09-28 23:30:44 +0530

    Decorator Pattern

---
+++ src/designpatterns/structural/decorator_pattern/DarkRoast.java
@@ -0,0 +1,13 @@
+
+public class DarkRoast extends Beverage {
+
+	public DarkRoast() {
+		description = "DarkRoast";
+	}
+
+	public double cost() {
+		return .99;
+	}
+
+}
+
---
+++ src/designpatterns/structural/decorator_pattern/SteamedMilk.java
@@ -0,0 +1,19 @@
+
+class SteamedMilk extends CondimentDecorator {
+
+	Beverage beverage;
+
+	public SteamedMilk(Beverage beverage) {
+		this.beverage = beverage;
+	}
+
+	public String getDescription() {
+		return beverage.getDescription() + ", Steamed Milk";
+	}
+
+	public double cost() {
+		return beverage.cost() + 0.10;
+	}
+
+}
+
