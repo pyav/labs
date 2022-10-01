9e948e7f74df0af406e292031918883cd39dcde6
pyav <verma.anand09@gmail.com>
2022-10-01 19:47:21 +0530

    Leetcode - Tree - easy

---
+++ src/leetcode/SumLeftNodes.java
@@ -0,0 +1,55 @@
+/**
+ * https://leetcode.com/problems/sum-of-left-leaves/
+ *
+ * Output:
+ * ------
+ * 24
+ * 4
+ */
+
+public class SumLeftNodes {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private int sum = 0;
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (null == root) {
+            return sum;
+        }
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            sum += root.left.val;
+        }
+        sumOfLeftLeaves(root.left);
+        sumOfLeftLeaves(root.right);
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(new SumLeftNodes().sumOfLeftLeaves(root));
+        TreeNode root2 = new TreeNode(1);
+        root2.left = new TreeNode(2);
+        root2.right = new TreeNode(3);
+        root2.left.left = new TreeNode(4);
+        root2.left.right = new TreeNode(5);
+        System.out.println(new SumLeftNodes().sumOfLeftLeaves(root2));
+    }
+
+}
+
