69421e06cdfd6acb9555d5e60b3b56253638800f
pyav <verma.anand09@gmail.com>
2022-10-01 20:07:50 +0530

    Leetcode - Tree - easy

---
+++ src/leetcode/BinarySearch.java
@@ -0,0 +1,48 @@
+/**
+ * https://leetcode.com/problems/search-in-a-binary-search-tree/
+ *
+ * Output:
+ * ------
+ * 2
+ */
+
+public class BinarySearch {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (val == root.val) {
+            return root;
+        }
+        if (val > root.val) {
+            return searchBST(root.right, val);
+        } else {
+            return searchBST(root.left, val);
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        System.out.println(new BinarySearch().searchBST(root, 2).val);
+    }
+
+}
+
