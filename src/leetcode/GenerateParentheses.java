5b8f11f7208c2d2c6d283e595e241922e9a643e3
Anand Verma <verma.anand09@gmail.com>
2026-09-16 13:54:28 +0530

    Leetcode: GenerateParentheses - medium

--- src/leetcode/GenerateParentheses.java
+++ src/leetcode/GenerateParentheses.java
@@ -11,33 +11,27 @@ import java.util.*;
 
 public class GenerateParentheses {
     public List<String> generateParenthesis(int n) {
-        Queue<String> q = new LinkedList<>();
-        q.add("()");
+        Set<String> current = new HashSet<>();
+        current.add("()");
         n--;
 
         while(n > 0) {
-            int size = q.size();
-            Set<String> set = new HashSet<>();
-            while(size > 0) {
-                String val = q.poll();
+            Set<String> next = new HashSet<>();
+            for (String val : current) {
                 for (int i = 0; i <= val.length(); i++) {
                     String creation = val.substring(0, i) + "()" + val.substring(i, val.length());
-                    if (set.contains(creation)) {
-                        continue;
-                    }
-                    set.add(creation);
-                    q.add(creation);
+                    next.add(creation);
                 }
-                size--;
             }
+            current = next;
             n--;
         }
 
-        return new ArrayList<>(q);
+        return new ArrayList<>(current);
     }
 
     public static void main(String[] args) {
-        List<String> result = new GenerateParentheses().generateParenthesis(2);
+        List<String> result = new GenerateParentheses().generateParenthesis(3);
         for (String val : result) {
             System.out.println(val);
         }
